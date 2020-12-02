95332c43735cd1658627a91e337871b0840ba8e2
Schoolbox_95 <bpudsey@qmail.qcc.edu>
2020-12-02 11:06:57 -0500

    shortening w stylus+pug

---
+++ src/MacintoshLoaderAbridged.svelte
@@ -0,0 +1,63 @@
+script
+    
+
+style
+   
+    spinner-size  75px !default
+    spinner-color  #69717d !default
+    overlay-color  black !default
+
+    absolute-center()
+        position  absolute
+        left  0
+        right  0
+        top  0
+        bottom  0
+        margin  auto
+    
+
+    .overlay 
+        background  rgba( overlay-color,.9)
+        absolute-center()
+    
+        
+    .spinner 
+        font-size   spinner-size
+        position  relative
+        display  inline-block
+        width  1em
+        height  1em
+
+        .center 
+            absolute-center()
+    
+    
+
+    .spinner-blade 
+        position  absolute
+        left  .4629em
+        bottom  0
+        width  .074em
+        height  .2777em
+        border-radius  .5em
+        background-color  transparent
+        transform-origin  center -.2222em
+        animation  spinner-fade 1s infinite linear
+
+
+        for i in 1 2 3 4 5 6 7 8 9 10 11 12 
+            :nth-child(i) 
+                animation-delay  .083 * (i-1)
+                transform  rotate(30deg * (i-1))
+            
+    @keyframes spinner-fade 
+        0% 
+            background-color   spinner-color
+        100% 
+            background-color  transparent
+    
+    
+.overlay
+    .spinner.center
+        for i in [...Array(12).keys]
+            .spinner-blade
---
+++ src/ShawbrookLoaderAbridged.svelte
@@ -0,0 +1,43 @@
+script
+    
+
+style
+    .shawbrook-loader 
+        width: 7rem
+        height: 4rem
+        display: flex
+        justify-content: center
+        background: linear-gradient(40deg,var(--clr0), #141414)
+        box-shadow: black 1px 1px
+    
+    .shawbrook-loader-dot 
+        position: absolute
+        transform-origin: bottom
+        width:1rem
+        height: 2rem
+        :after    
+            content: ''
+            position: absolute
+            width: 1rem
+            height: 1rem
+            background: radial-gradient(var(--clr2), var(--clr2) 0.1rem, transparent 0.1rem)
+            animation: 1.6s pulse infinite
+        
+    for i in 1 2 3 4 5 6 7 8
+
+        .shawbrook-loader-dot
+            :nth-child(i) 
+                animation-delay:i*0.2s
+                transform: rotate(i*45deg)
+
+            :nth-child(1):after 
+                animation-delay:calc(i*0.2s)
+    
+    @keyframes pulse
+        0% transform: scale(1)
+        40% transform: scale(2.4)
+        100% transform: scale(1)
+
+.shawbrook-loader
+    for x in [...Array(8),keys]
+        .shawbrook-loader-dot
